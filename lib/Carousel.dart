9a2a24c14e3f336f558df4e15f354ab28355402e
Utkarsh2524992 <utkarsh2524992@gmail.com>
2021-02-02 23:26:23 +0530

    Club Card reformatted

--- lib/Carousel.dart
+++ lib/Carousel.dart
@@ -20,14 +20,16 @@ class _CarouselState extends State<Carousel> {
 
     return Container(
       margin: EdgeInsets.only(left: size.width / 50, right: size.width / 50),
-      height: 250,
+    //  height: 250,
+      height: 185,
       child: ListView.builder(
         itemCount: widget.Clubs.length,
         scrollDirection: Axis.horizontal,
         // children: <Widget>[
         itemBuilder: (context, index) {
           return Container(
-            width: 200.0,
+            //width: 200.0,
+            width: 150,
             child: Card(
               elevation: 10,
               shadowColor: widget.shadow.withOpacity(0.2),
@@ -39,11 +41,16 @@ class _CarouselState extends State<Carousel> {
                 },
                 child: Wrap(
                   children: <Widget>[
-                    Image.network(
-                      widget.Clubs[index].clubAvatar,
-                      height: 130,
-                      width: 200,
-                      fit: BoxFit.cover,
+                    Container(
+                      height: 80,
+                      child: Center(
+                        child: Image.network(
+                          widget.Clubs[index].clubAvatar,
+                //      height: 130,
+                //      width: 200,
+                          fit: BoxFit.cover,
+                        ),
+                      ),
                     ),
                     ListTile(
                       title: Text(
@@ -51,13 +58,70 @@ class _CarouselState extends State<Carousel> {
                         style: TextStyle(
                             fontFamily: 'Lato', fontWeight: FontWeight.bold),
                       ),
-                      subtitle: Text(
-                          widget.Clubs[index].description != null
-                              ? widget.Clubs[index].description
-                              : 'There is no description for this club.',
-                          style: TextStyle(
-                            fontFamily: 'Lato',
-                          )),
+                      subtitle:
+                        Column(
+                          children: [
+                            Row(
+                              children: [
+                                CircleAvatar(
+                                backgroundImage: NetworkImage(widget.Clubs[index].creator.avatar),
+                                radius: 10,
+                              ),
+                                SizedBox(width: 5,),
+
+                                Text(
+                                  widget.Clubs[index].creator.username,
+                                style: TextStyle(
+                                  fontFamily: "Lato",
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 10,
+                                  color: Colors.redAccent
+                                ),
+                              ),]
+                            ),
+                       Container(
+                         margin: EdgeInsets.fromLTRB(0, 30, 0, 0),
+                         child: Row(
+                           mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                           children: [
+                             Text("12.1K LISTENERS",style: TextStyle(
+                               fontFamily: "Lato",
+                               fontSize: size.width/50,
+                               color: Colors.grey
+                             ),),
+                             Container(
+                                color: Colors.red,
+                                alignment: Alignment(1, 1),
+                                padding: EdgeInsets.all(2),
+                                child: Text(
+                                  "LIVE",
+                                  style: TextStyle(
+                                      fontFamily: 'Lato',
+                                      fontWeight: FontWeight.bold,
+                                      color: Colors.white,
+                                      letterSpacing: 2.0,
+                                    fontSize: size.width/50
+                                  ),
+                                ),
+                              ),
+                           ],
+                         ),
+                       ),
+                          ],
+                        ),
+//                      trailing:  Container(
+//                        color: Colors.red,
+//                        child: Text(
+//                          "LIVE",
+//                          style: TextStyle(
+//                              fontFamily: 'Lato',
+//                              fontWeight: FontWeight.bold,
+//                              color: Colors.white,
+//                              letterSpacing: 2.0,
+//                            fontSize: 10
+//                          ),
+//                        ),
+//                      ),
                     ),
                   ],
                 ),
