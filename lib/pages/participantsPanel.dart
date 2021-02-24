d25f3c371b83f10d2755d02aa4feada5290f73d6
Utkarsh2524992 <utkarsh2524992@gmail.com>
2021-02-25 03:02:22 +0530

    MOOT-84 Done:Panelist panel upgraded

--- lib/pages/participantsPanel.dart
+++ lib/pages/participantsPanel.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:mootclub_app/Models/built_post.dart';
 import 'package:mootclub_app/providers/userData.dart';
@@ -39,7 +40,7 @@ class ParticipantsPanel extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final userId = Provider.of<UserData>(context, listen: false).userId;
-
+    final cuser = Provider.of<UserData>(context,listen: false).user;
     return SlidingUpPanel(
       minHeight: size.height / 20,
       maxHeight: size.height / 1.5,
@@ -54,7 +55,7 @@ class ParticipantsPanel extends StatelessWidget {
             height: size.height / 30,
           ),
           Center(
-            child: Text("Panelists",
+            child: Text("Host",
                 style: TextStyle(
                     fontFamily: 'Lato',
                     fontWeight: FontWeight.bold,
@@ -66,65 +67,129 @@ class ParticipantsPanel extends StatelessWidget {
           ),
           Container(
             margin: EdgeInsets.fromLTRB(10, 10, 0, 0),
-            height: MediaQuery.of(context).size.height / 2 + 40,
-            child: GridView.builder(
-              itemCount: participantList.length,
-              gridDelegate:
-                  SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 3),
-              itemBuilder: (context, index) {
-                final participantId = participantList[index].userId;
-                return Container(
-                  child: Stack(
+            child: Stack(
+              children: [
+                InkWell(
+                  onTap: (){
+                    Navigator.of(context).push(MaterialPageRoute(builder: (_)=>ProfilePage(userId: userId,)));
+                  },
+                  child: Column(
                     children: [
-                      InkWell(
-                        onTap: (){
-                          Navigator.of(context).push(MaterialPageRoute(builder: (_)=>ProfilePage(userId: participantList[index].userId,)));
-                        },
-                        child: Column(
-                          children: [
-                            CircleAvatar(
-                              radius: size.width / 9.4,
-                              backgroundColor: Color(0xffFDCF09),
-                              child: CircleAvatar(
-                                radius: size.width / 10,
-                                backgroundImage:
-                                    NetworkImage(participantList[index].avatar),
-                              ),
-                            ),
-                            Text(
-                              participantList[index].username,
-                              style: TextStyle(
-                                  fontFamily: "Lato",
-                                  fontWeight: FontWeight.bold),
-                            ),
-                          ],
+                      CircleAvatar(
+                        radius: size.width / 9.4,
+                        backgroundColor: Color(0xffFDCF09),
+                        child: CircleAvatar(
+                          radius: size.width / 10,
+                          backgroundImage:
+                          NetworkImage(cuser.avatar),
                         ),
                       ),
-                      // display menu to owner only.
-                      if (isOwner && participantId != userId)
-                        Positioned(
-                          right: 10,
-                          child: PopupMenuButton<String>(
-                            onSelected: (val) =>
-                                _handleMenuButtons(val, participantId),
-                            itemBuilder: (BuildContext context) {
-                              return {
-                                'Mute',
-                                'Remove',
-                                'Block',
-                              }.map((String choice) {
-                                return PopupMenuItem<String>(
-                                  value: choice,
-                                  child: Text(choice),
-                                );
-                              }).toList();
-                            },
-                          ),
-                        )
+                      Text(
+                        cuser.username,
+                        style: TextStyle(
+                            fontFamily: "Lato",
+                            fontWeight: FontWeight.bold),
+                      ),
                     ],
                   ),
-                );
-              },
+                ),
+              ],
+            ),
+          ),
+          SizedBox(
+            height: size.height / 30,
+          ),
+          Center(
+            child: Text("Panelists",
+                style: TextStyle(
+                    fontFamily: 'Lato',
+                    fontWeight: FontWeight.bold,
+                    fontSize: size.width / 20,
+                    color: Colors.redAccent)),
+          ),
+          SizedBox(
+            height: size.height / 50,
+          ),
+          Expanded(
+            child: Container(
+              margin: EdgeInsets.fromLTRB(10, 10, 0, 0),
+              child: GridView.builder(
+                itemCount: participantList.where((element) => element.userId!=cuser.userId).length+1,
+                gridDelegate:
+                    SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 3),
+                itemBuilder: (context, index) {
+                  final participantId = participantList[index].userId;
+                  return index!=participantList.where((element) => element.userId!=cuser.userId).length?
+                  Container(
+                    child: Stack(
+                      children: [
+                        InkWell(
+                          onTap: (){
+                            Navigator.of(context).push(MaterialPageRoute(builder: (_)=>ProfilePage(userId: participantList[index].userId,)));
+                          },
+                          child: Column(
+                            children: [
+                              CircleAvatar(
+                                radius: size.width / 9.4,
+                                backgroundColor: Color(0xffFDCF09),
+                                child: CircleAvatar(
+                                  radius: size.width / 10,
+                                  backgroundImage:
+                                      NetworkImage(participantList[index].avatar),
+                                ),
+                              ),
+                              Text(
+                                participantList[index].username,
+                                style: TextStyle(
+                                    fontFamily: "Lato",
+                                    fontWeight: FontWeight.bold),
+                              ),
+                            ],
+                          ),
+                        ),
+                        // display menu to owner only.
+                        if (isOwner && participantId != userId)
+                          Positioned(
+                            right: 10,
+                            child: PopupMenuButton<String>(
+                              onSelected: (val) =>
+                                  _handleMenuButtons(val, participantId),
+                              itemBuilder: (BuildContext context) {
+                                return {
+                                  'Mute',
+                                  'Remove',
+                                  'Block',
+                                }.map((String choice) {
+                                  return PopupMenuItem<String>(
+                                    value: choice,
+                                    child: Text(choice),
+                                  );
+                                }).toList();
+                              },
+                            ),
+                          )
+                      ],
+                    ),
+                  ):
+                  InkWell(
+                    onTap: (){
+                    },
+                    child: Container(
+                      margin: EdgeInsets.all(size.width/20),
+                      child: Icon(
+                        Icons.person_add,
+                        color: Colors.redAccent,
+                        size: size.width/10,
+                      ),
+                      decoration: new BoxDecoration(
+                        shape: BoxShape.circle,
+                        color: Colors.white,
+                        border: Border.all(color: Colors.redAccent,width: 2)
+                      ),
+                    ),
+                  );
+                },
+              ),
             ),
           )
         ],
