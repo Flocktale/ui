6ac2126ae150a0ebccc70821720d0dbf8a3b0390
Utkarsh2524992 <utkarsh2524992@gmail.com>
2021-03-01 21:28:57 +0530

    MOOT-66 done:Audience Screen added

--- lib/pages/AudiencePage.dart
+++ lib/pages/AudiencePage.dart
@@ -1,9 +1,13 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 import 'package:mootclub_app/Models/built_post.dart';
-import 'package:mootclub_app/services/chopper/database_api_service.dart';
 import 'package:mootclub_app/providers/userData.dart';
+import 'package:mootclub_app/services/chopper/database_api_service.dart';
 import 'package:provider/provider.dart';
+
 import 'ProfilePage.dart';
+
 class AudiencePage extends StatefulWidget {
   BuiltClub club;
   AudiencePage({this.club});
@@ -12,108 +16,171 @@ class AudiencePage extends StatefulWidget {
 }
 
 class _AudiencePageState extends State<AudiencePage> {
-  bool _isSearching = false;
   Map<String, dynamic> audienceMap = {
     'data': null,
     'isLoading': true,
   };
 
-  Future<void> _fetchAudienceData() async{
-    final service = Provider.of<DatabaseApiService>(context,listen:false);
-    final authToken = Provider.of<UserData>(context,listen: false).authToken;
-    audienceMap['data'] = await service.getAudienceList(clubId: widget.club.clubId, lastevaluatedkey: (audienceMap['data'] as BuiltSearchUsers)?.lastevaluatedkey, authorization: authToken);
+  Future<void> _fetchAudienceData() async {
+    final authToken = (Provider.of<UserData>(context, listen: false).authToken);
+    audienceMap['data'] =
+        (await Provider.of<DatabaseApiService>(context, listen: false)
+                .getAudienceList(
+                    clubId: widget.club.clubId,
+                    lastevaluatedkey: (audienceMap['data'] as BuiltAudienceList)
+                        ?.lastevaluatedkey,
+                    authorization: authToken))
+            .body;
     audienceMap['isLoading'] = false;
-    setState(() {
-    });
+    setState(() {});
   }
 
-  Future<void> _fetchMoreAudienceData() async{
-    if(audienceMap['isLoading']==true)
-      return;
-    setState(() {
-    });
-    final lastEvaluatedKey = (audienceMap['data'] as BuiltSearchUsers)?.lastevaluatedkey;
-    if(lastEvaluatedKey!=null){
+  void _fetchMoreAudienceData() async {
+    if (audienceMap['isLoading'] == true) return;
+    setState(() {});
+
+    final lastEvaluatedKey =
+        (audienceMap['data'] as BuiltAudienceList)?.lastevaluatedkey;
+
+    if (lastEvaluatedKey != null) {
       await _fetchAudienceData();
-    }
-    else{
+    } else {
       await Future.delayed(Duration(milliseconds: 200));
       audienceMap['isLoading'] = false;
     }
-    setState(() {
-    });
+    setState(() {});
   }
 
-  Widget _audienceListWidget(){
-    final audienceUsers = (audienceMap['data'] as BuiltSearchUsers)?.users;
+  Widget audienceList() {
+    final size = MediaQuery.of(context).size;
+    final audienceListUsers =
+        (audienceMap['data'] as BuiltAudienceList)?.audience;
     final bool isLoading = audienceMap['isLoading'];
-    final listLength = (audienceUsers?.length ?? 0) + 1;
-    return NotificationListener<ScrollNotification>(
-      onNotification: (ScrollNotification scrollInfo) {
-        if (scrollInfo.metrics.pixels == scrollInfo.metrics.maxScrollExtent) {
-          audienceMap['isLoading'] = true;
-          _fetchMoreAudienceData();
-        }
-        return true;
-      },
-      child: ListView.builder(
-        shrinkWrap: true,
-        itemCount: listLength,
-          itemBuilder: (context,index){
-            if (index == listLength - 1) {
-              if (isLoading)
-                return Container(
-                  margin: EdgeInsets.fromLTRB(10, 0, 10, 10),
-                  child: Center(child: CircularProgressIndicator()),
-                );
-              else
-                return Container();
+    final listLength = (audienceListUsers?.length ?? 0) + 1;
+    return Container(
+        margin: EdgeInsets.fromLTRB(10, 10, 10, 0),
+        child: NotificationListener<ScrollNotification>(
+          onNotification: (ScrollNotification scrollInfo) {
+            if (scrollInfo.metrics.pixels ==
+                scrollInfo.metrics.maxScrollExtent) {
+              audienceMap['isLoading'] = true;
             }
-            final _user = audienceUsers[index];
-            return Container(
-              key: ValueKey(_user.username),
-              margin: EdgeInsets.fromLTRB(10, 0, 10, 10),
-              child: ListTile(
-                leading: CircleAvatar(
-                  backgroundImage: NetworkImage(_user.avatar),
-                ),
-                title: InkWell(
-                  onTap: (){
-                    Navigator.of(context).push(MaterialPageRoute(builder: (_)=>ProfilePage(userId: _user.userId,)));
-                  },
-                  child: Text(
-                    _user.username,
-                    style: TextStyle(
-                      fontFamily: "Lato",
+            return true;
+          },
+          child: ListView.builder(
+              itemCount: listLength,
+              shrinkWrap: true,
+              itemBuilder: (context, index) {
+                if (index == listLength - 1) {
+                  if (isLoading)
+                    return Container(
+                      margin: EdgeInsets.fromLTRB(10, 0, 10, 10),
+                      child: Center(child: CircularProgressIndicator()),
+                    );
+                  else
+                    return Container();
+                }
+
+                final _user = audienceListUsers[index].audience;
+
+                return Container(
+                  key: ValueKey(_user.username),
+                  child: ListTile(
+                    leading: CircleAvatar(
+                      backgroundImage: NetworkImage(_user.avatar),
+                    ),
+                    title: InkWell(
+                      onTap: () {
+                        Navigator.of(context).push(MaterialPageRoute(
+                            builder: (_) => ProfilePage(
+                                  userId: _user.userId,
+                                )));
+                      },
+                      child: Text(
+                        _user.username,
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                            fontFamily: 'Lato', fontWeight: FontWeight.bold),
+                      ),
+                    ),
+                    subtitle: InkWell(
+                      onTap: () {
+                        Navigator.of(context).push(MaterialPageRoute(
+                            builder: (_) => ProfilePage(
+                                  userId: _user.userId,
+                                )));
+                      },
+                      child: Text(
+                        _user.tagline != null ? '@${_user.tagline}' : '',
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                          fontFamily: 'Lato',
+                        ),
+                      ),
+                    ),
+                    trailing: ButtonTheme(
+                      minWidth: size.width / 3.5,
+                      child: RaisedButton(
+                        onPressed: () async {
+                          final authToken =
+                              Provider.of<UserData>(context, listen: false)
+                                  .authToken;
+                          final resp = (await Provider.of<DatabaseApiService>(
+                                  context,
+                                  listen: false)
+                              .blockAudience(
+                                  clubId: widget.club.clubId,
+                                  audienceId: _user.userId,
+                                  authorization: authToken));
+                          if (resp.isSuccessful) {
+                            setState(() {});
+                            Navigator.of(context)
+                                .popUntil((route) => route.isFirst);
+                          } else {
+                            Fluttertoast.showToast(msg: 'Something went wrong');
+                          }
+                        },
+                        color: Colors.red[600],
+                        child: Text('Kick Out',
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontWeight: FontWeight.bold,
+                              fontFamily: 'Lato',
+                            )),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(5.0),
+                          //side: BorderSide(color: Colors.red[600]),
+                        ),
+                      ),
                     ),
                   ),
-                ),
-              ),
-            );
-
-      }),
-    );
+                );
+              }),
+        ));
   }
+
   @override
   void initState() {
-    _fetchAudienceData();
     super.initState();
+    _fetchAudienceData();
   }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(
-          "Audience",
-          style: TextStyle(
-            fontFamily: "Lato",
-            color: Colors.black
-          ),
-        ),
-        iconTheme: IconThemeData(color: Colors.black),
-        backgroundColor: Colors.white,
+    return SafeArea(
+      child: Scaffold(
+        appBar: AppBar(
+            backgroundColor: Colors.white,
+            iconTheme: IconThemeData(color: Colors.black),
+            title: Text(
+              "Audience",
+              style: TextStyle(
+                  fontFamily: "Lato",
+                  color: Colors.black,
+                  fontWeight: FontWeight.bold),
+            )),
+        body: audienceList(),
       ),
-      body: _audienceListWidget(),
     );
   }
 }
