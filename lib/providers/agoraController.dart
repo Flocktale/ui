58023b5820aad937e694304a4da890d07e3fb77e
mohit-mangal <mohitmangal.ece18@itbhu.ac.in>
2021-03-07 18:26:12 +0530

    agora handler updated

--- lib/providers/agoraController.dart
+++ lib/providers/agoraController.dart
@@ -1,9 +1,14 @@
 import 'package:agora_handler/main.dart';
+import 'package:agora_rtc_engine/rtc_engine.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:mootclub_app/Models/built_post.dart';
 
 class AgoraController {
   AgoraHandler _agoraHandler;
+
+  /// use this handler to listen to various callbacks related to agora.
+  RtcEngineEventHandler agoraEventHandler;
+
   bool _isClubMuted;
 
   bool isMicMuted;
@@ -15,7 +20,7 @@ class AgoraController {
     _agoraHandler = AgoraHandler();
     _isClubMuted = false;
     isMicMuted = true;
-    _agoraHandler.init();
+    _agoraHandler.init().then((handler) => agoraEventHandler = handler);
     return this;
   }
 
@@ -24,17 +29,21 @@ class AgoraController {
   Future<void> joinAsAudience({
     @required String clubId,
     @required String token,
+    @required int intergerUsername,
   }) async {
     assert(clubId != null && token != null);
-    await _agoraHandler.joinClub(clubId, token);
+    await _agoraHandler.joinClub(clubId, token,
+        integerUsername: intergerUsername ?? 0);
   }
 
   Future<void> joinAsParticipant({
     @required String clubId,
     @required String token,
+    @required int intergerUsername,
   }) async {
     assert(clubId != null && token != null);
-    await _agoraHandler.joinClub(clubId, token, isHost: true);
+    await _agoraHandler.joinClub(clubId, token,
+        isHost: true, integerUsername: intergerUsername ?? 0);
   }
 
   Future<void> stop() async {
