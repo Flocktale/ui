18da757203da6e1dbdd15b6177abf007e5765054
Utkarsh2524992 <utkarsh2524992@gmail.com>
2021-03-31 01:50:17 +0530

    UI changes on community page

--- lib/Widgets/CommunityCard.dart
+++ lib/Widgets/CommunityCard.dart
@@ -1,6 +1,7 @@
 import 'dart:ui';
 
 import 'package:flocktale/Widgets/customImage.dart';
+import 'package:flocktale/pages/CommunityPage.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
@@ -18,225 +19,230 @@ class _CommunityCardState extends State<CommunityCard> {
   Widget build(BuildContext context) {
     final _userAvatar = Provider.of<UserData>(context,listen:false).user.avatar;
     final size = MediaQuery.of(context).size;
-    return ClipRRect(
-      child: Container(
-        margin: EdgeInsets.fromLTRB(10, 10, 10, 0),
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.all(Radius.circular(10))
-        ),
-        child: Stack(children: <Widget>[
-          Container(
-            decoration: BoxDecoration(
-              color: Colors.transparent,
-              borderRadius: BorderRadius.all(Radius.circular(10)),
-              image: DecorationImage(
-                fit: BoxFit.fill,
-                image: AssetImage(
-                  'assets/images/logo.png',
+    return InkWell(
+      onTap: (){
+        Navigator.of(context).push(MaterialPageRoute(builder: (_)=>CommunityPage()));
+      },
+      child: ClipRRect(
+        child: Container(
+          margin: EdgeInsets.fromLTRB(10, 10, 10, 0),
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.all(Radius.circular(10))
+          ),
+          child: Stack(children: <Widget>[
+            Container(
+              decoration: BoxDecoration(
+                color: Colors.transparent,
+                borderRadius: BorderRadius.all(Radius.circular(10)),
+                image: DecorationImage(
+                  fit: BoxFit.fill,
+                  image: AssetImage(
+                    'assets/images/logo.png',
+                  ),
                 ),
               ),
+              height: size.height/4,
             ),
-            height: size.height/4,
-          ),
-          Container(
-            height: size.height/4,
-            width: size.width,
-            decoration: BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.only(bottomLeft: Radius.circular(10),bottomRight: Radius.circular(10)),
-                gradient: LinearGradient(
-                    begin: FractionalOffset.topCenter,
-                    end: FractionalOffset.bottomCenter,
-                    colors: [
-                      Colors.grey.withOpacity(0.2),
-                      Colors.black,
-                    ],
-                    stops: [
-                      0.0,
-                      1.0
-                    ])),
-          ),
-          Positioned(
-            bottom: size.height/15,
-            left: 10,
-            child: Row(
-              children: [
-                CircleAvatar(
-                  backgroundColor: Colors.white,
-                  radius: size.width/20,
-                  child: CircleAvatar(
-                backgroundImage: NetworkImage(_userAvatar),
-                    radius: size.width/23,
-            ),
-                ),
-                SizedBox(
-                  width: size.width/30,
-                ),
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      "COMMUNITY NAME",
-                      style: TextStyle(
-                          fontFamily: "Lato",
-                          color: Colors.white,
-                          letterSpacing: 2.0
-                      ),
-                    ),
-                    Text(
-                      "Creator name",
-                      style: TextStyle(
-                        fontFamily: "Lato",
-                        color: Colors.grey[400],
-                        fontSize: size.width/30
-                      ),
-                    ),
-                  ],
-                ),
-              ],
-            ),
-          ),
-          Positioned(
-            bottom: 10,
-            left: 10,
-            child: Container(
-              height: size.height/22.5,
+            Container(
+              height: size.height/4,
               width: size.width,
-              child: ListView.builder(
-                  scrollDirection: Axis.horizontal,
-                  itemCount: 5,
-                  itemBuilder: (context,index){
-                    return index==4?
+              decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.only(bottomLeft: Radius.circular(10),bottomRight: Radius.circular(10)),
+                  gradient: LinearGradient(
+                      begin: FractionalOffset.topCenter,
+                      end: FractionalOffset.bottomCenter,
+                      colors: [
+                        Colors.grey.withOpacity(0.2),
+                        Colors.black,
+                      ],
+                      stops: [
+                        0.0,
+                        1.0
+                      ])),
+            ),
+            Positioned(
+              bottom: size.height/15,
+              left: 10,
+              child: Row(
+                children: [
+                  CircleAvatar(
+                    backgroundColor: Colors.white,
+                    radius: size.width/20,
+                    child: CircleAvatar(
+                  backgroundImage: NetworkImage(_userAvatar),
+                      radius: size.width/23,
+              ),
+                  ),
+                  SizedBox(
+                    width: size.width/30,
+                  ),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        "COMMUNITY NAME",
+                        style: TextStyle(
+                            fontFamily: "Lato",
+                            color: Colors.white,
+                            letterSpacing: 2.0
+                        ),
+                      ),
+                      Text(
+                        "Creator name",
+                        style: TextStyle(
+                          fontFamily: "Lato",
+                          color: Colors.grey[400],
+                          fontSize: size.width/30
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+            Positioned(
+              bottom: 10,
+              left: 10,
+              child: Container(
+                height: size.height/22.5,
+                width: size.width,
+                child: ListView.builder(
+                    scrollDirection: Axis.horizontal,
+                    itemCount: 5,
+                    itemBuilder: (context,index){
+                      return index==4?
+                          Container(
+                            margin: EdgeInsets.fromLTRB(0, 0, 5, 0),
+                            height: size.height/22.5,
+                            width: size.height/22.5,
+                            decoration: BoxDecoration(
+                              //color: Color(0xfff74040)
+                              color: Colors.black45
+                            ),
+                            child: Center(
+                              child: Text(
+                                "+6",
+                                style: TextStyle(
+                                  fontFamily: "Lato",
+                                  color: Colors.white
+                                ),
+                              ),
+                            ),
+                          ):
+                      Container(
+                        margin: EdgeInsets.fromLTRB(0, 0, 5, 0),
+                        height: size.height/22.5,
+                        width: size.height/22.5,
+                        decoration: BoxDecoration(
+                            borderRadius: BorderRadius.all(Radius.circular(2)),
+                            image: DecorationImage(
+                                image: NetworkImage(
+                                    _userAvatar
+                                )
+                            )
+                        ),
+                      );
+                    }),
+              ),
+            ),
+            Positioned(
+              bottom: 10,
+              right: 10,
+              child: Column(
+                children: [
+                  FittedBox(
+                    child: Row(
+                      children: [
+                        Text(
+                          "15",
+                          style: TextStyle(
+                              fontFamily: "Lato",
+                              color: Colors.white
+                          ),
+                        ),
+                        SizedBox(
+                          width: 2,
+                        ),
                         Container(
-                          margin: EdgeInsets.fromLTRB(0, 0, 5, 0),
-                          height: size.height/22.5,
-                          width: size.height/22.5,
+                          height: size.height/50,
+                          width: size.width/15,
                           decoration: BoxDecoration(
-                            //color: Color(0xfff74040)
-                            color: Colors.black45
+                              color: Color(0xfff74040)
                           ),
                           child: Center(
                             child: Text(
-                              "+6",
+                              "LIVE",
                               style: TextStyle(
-                                fontFamily: "Lato",
-                                color: Colors.white
+                                  fontFamily: "Lato",
+                                  fontWeight: FontWeight.bold,
+                                  letterSpacing: 2.0,
+                                  color: Colors.white,
+                                  fontSize: size.width/50
                               ),
                             ),
                           ),
-                        ):
-                    Container(
-                      margin: EdgeInsets.fromLTRB(0, 0, 5, 0),
-                      height: size.height/22.5,
-                      width: size.height/22.5,
-                      decoration: BoxDecoration(
-                          borderRadius: BorderRadius.all(Radius.circular(2)),
-                          image: DecorationImage(
-                              image: NetworkImage(
-                                  _userAvatar
-                              )
-                          )
-                      ),
-                    );
-                  }),
-            ),
-          ),
-          Positioned(
-            bottom: 10,
-            right: 10,
-            child: Column(
-              children: [
-                FittedBox(
-                  child: Row(
-                    children: [
-                      Text(
-                        "15",
-                        style: TextStyle(
-                            fontFamily: "Lato",
-                            color: Colors.white
-                        ),
-                      ),
-                      SizedBox(
-                        width: 2,
-                      ),
-                      Container(
-                        height: size.height/50,
-                        width: size.width/15,
-                        decoration: BoxDecoration(
-                            color: Color(0xfff74040)
-                        ),
-                        child: Center(
-                          child: Text(
-                            "LIVE",
-                            style: TextStyle(
-                                fontFamily: "Lato",
-                                fontWeight: FontWeight.bold,
-                                letterSpacing: 2.0,
-                                color: Colors.white,
-                                fontSize: size.width/50
-                            ),
-                          ),
-                        ),
-                      )
-                    ],
-                  ),
-                ),
-                FittedBox(
-                  child: Row(
-                    children: [
-                      Text(
-                          "35",
-                        style: TextStyle(
-                          fontFamily: "Lato",
-                          color: Colors.white
-                        ),
-                      ),
-                      SizedBox(
-                        width: 2,
-                      ),
-                      Icon(
-                          Icons.schedule,
-                        color: Colors.white,
-                      )
-                    ],
-                  ),
-                )
-              ],
-            ),
-          ),
-          Positioned(
-            top: 0,
-            right: 0,
-            child: Container(
-              decoration: BoxDecoration(
-                color: Colors.black.withOpacity(0.4),
-                borderRadius: BorderRadius.only(topRight: Radius.circular(10), bottomLeft: Radius.circular(10))
-              ),
-              child: Row(
-                children: [
-                  Text(
-                  "15K",
-                    style: TextStyle(
-                      fontFamily: "Lato",
-                      color: Colors.white,
-                      fontSize: size.width/30
+                        )
+                      ],
                     ),
                   ),
-                  Icon(
-                    Icons.person,
-                    color: Colors.white,
-                    size: size.width/25,
-                  ),
-                  IconButton(icon: isMember?Icon(Icons.check,color: Colors.green,):Icon(Icons.add,color: Colors.white,), onPressed: (){
-                    setState(() {
-                      isMember = !isMember;
-                    });
-                  })
+                  FittedBox(
+                    child: Row(
+                      children: [
+                        Text(
+                            "35",
+                          style: TextStyle(
+                            fontFamily: "Lato",
+                            color: Colors.white
+                          ),
+                        ),
+                        SizedBox(
+                          width: 2,
+                        ),
+                        Icon(
+                            Icons.schedule,
+                          color: Colors.white,
+                        )
+                      ],
+                    ),
+                  )
                 ],
               ),
+            ),
+            Positioned(
+              top: 0,
+              right: 0,
+              child: Container(
+                decoration: BoxDecoration(
+                  color: Colors.black.withOpacity(0.4),
+                  borderRadius: BorderRadius.only(topRight: Radius.circular(10), bottomLeft: Radius.circular(10))
+                ),
+                child: Row(
+                  children: [
+                    Text(
+                    "15K",
+                      style: TextStyle(
+                        fontFamily: "Lato",
+                        color: Colors.white,
+                        fontSize: size.width/30
+                      ),
+                    ),
+                    Icon(
+                      Icons.person,
+                      color: Colors.white,
+                      size: size.width/25,
+                    ),
+                    IconButton(icon: isMember?Icon(Icons.check,color: Colors.green,):Icon(Icons.add,color: Colors.white,), onPressed: (){
+                      setState(() {
+                        isMember = !isMember;
+                      });
+                    })
+                  ],
+                ),
+              )
             )
-          )
-        ]),
+          ]),
+        ),
       ),
     );
   }
---
+++ lib/pages/CommunityPage.dart
@@ -0,0 +1,239 @@
+import 'package:flocktale/Widgets/customImage.dart';
+import 'package:flutter/material.dart';
+class CommunityPage extends StatefulWidget {
+  @override
+  _CommunityPageState createState() => _CommunityPageState();
+}
+
+class _CommunityPageState extends State<CommunityPage> with TickerProviderStateMixin{
+  TabController _tabController;
+  bool _isOwner = true;
+  Widget tabPage(int index){
+    final size = MediaQuery.of(context).size;
+    if(index==0){
+      return Column(
+        children: [
+          Container(
+            height: size.height/6,
+            width: size.width,
+            decoration: BoxDecoration(
+                image: DecorationImage(
+                    image: AssetImage("assets/images/logo.png"),
+                    fit: BoxFit.cover
+                )
+            ),
+          ),
+          Container(
+            padding: EdgeInsets.fromLTRB(size.width/20, size.width/20, size.width/20, size.width/20),
+            color: Colors.grey[200],
+            child: Row(
+              children: [
+                CircleAvatar(
+                  backgroundImage: AssetImage(
+                    "assets/images/logo.png",
+                  ),
+                  radius: size.width/15,
+                ),
+                SizedBox(
+                  width: size.width/20,
+                ),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      "COMMUNITY NAME",
+                      style: TextStyle(
+                          fontFamily: "Lato",
+                          fontWeight: FontWeight.bold
+                      ),
+                    ),
+                    Text(
+                      "Creator name",
+                      style: TextStyle(
+                          fontFamily: "Lato",
+                          color: Colors.grey
+                      ),
+                    )
+                  ],
+                )
+              ],
+            ),
+          ),
+          Container(
+              height: size.height/8,
+              color: Colors.grey[200],
+              padding: EdgeInsets.fromLTRB(0, 0, 0, size.width/20),
+              child: ListView.builder(
+                  itemCount: 10,
+                  scrollDirection: Axis.horizontal,
+                  itemBuilder: (context,index){
+                    return Container(
+                      margin:EdgeInsets.fromLTRB(size.width/20,0,0,0),
+                      child: Column(
+                        children: [
+                          CircleAvatar(
+                            backgroundImage: AssetImage("assets/images/logo.png"),
+                            radius: size.width/15,
+                          ),
+                          Container(
+                            width: size.width/10,
+                            child: FittedBox(
+                              child: Text(
+                                "Name ${index+1}",
+                                style: TextStyle(
+                                    fontFamily: "Lato",
+                                    fontWeight: FontWeight.bold
+                                ),
+                              ),
+                            ),
+                          )
+                        ],
+                      ),
+                    );
+                  })
+          )
+        ],
+      );
+    }
+    else{
+      return Container();
+    }
+  }
+
+  void _justRefresh([Function refresh]) {
+    if (this.mounted) {
+      if (refresh != null) {
+        refresh();
+      }
+      setState(() {});
+    }
+  }
+
+  _navigateTo(Widget page) async {
+    await Navigator.of(context).push(
+      MaterialPageRoute(
+        builder: (_) => page,
+      ),
+    );
+    _justRefresh();
+  }
+
+  Future<void> _handleMenuButtons(String value) async {
+    switch (value) {
+      case 'Join Requests':
+        // await _navigateTo(ClubJoinRequests(club: widget.club));
+        break;
+
+      case 'Option 1':
+        // await _navigateTo(InviteScreen(
+        //   club: widget.club,
+        //   forPanelist: true,
+        // ));
+        break;
+
+      case 'Option 2':
+        // await _navigateTo(InviteScreen(
+        //   club: widget.club,
+        //   forPanelist: false,
+        // ));
+        break;
+
+      case 'Option 3':
+        // await _navigateTo(BlockedUsersPage(club: widget.club));
+        break;
+    }
+  }
+
+  Widget _showMenuButtons() {
+    return PopupMenuButton<String>(
+      onSelected: _handleMenuButtons,
+      itemBuilder: (BuildContext context) {
+        return _isOwner
+            ? {'Option 1', 'Option 2', 'Option 3'}
+            .map((String choice) {
+          return PopupMenuItem<String>(
+            value: choice,
+            child: Text(choice),
+          );
+        }).toList()
+            : {'Option 1'}.map((String choice) {
+          return PopupMenuItem<String>(
+            value: choice,
+            child: Text(choice),
+          );
+        }).toList();
+      },
+    );
+  }
+
+  @override
+  void initState() {
+    _tabController = TabController(length: 4, vsync: this, initialIndex: 0);
+    super.initState();
+  }
+  @override
+  Widget build(BuildContext context) {
+    return DefaultTabController(
+      initialIndex: 0,
+      length: 4,
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text(
+            "Community Name",
+            style: TextStyle(
+              fontFamily: "Lato",
+              fontWeight: FontWeight.bold
+            ),
+          ),
+          actions: [
+            _showMenuButtons()
+          ],
+          bottom: TabBar(
+            controller: _tabController,
+            isScrollable: true,
+            unselectedLabelColor: Colors.grey[700],
+            labelColor: Colors.white,
+            labelStyle:
+            TextStyle(fontFamily: 'Lato', fontWeight: FontWeight.bold),
+            indicator: UnderlineTabIndicator(
+              borderSide: BorderSide(color: Colors.white),
+            ),
+            indicatorSize: TabBarIndicatorSize.tab,
+            tabs: [
+              Text(
+                "HOME",
+                style: TextStyle(
+                    fontFamily: "Lato",
+                    letterSpacing: 2.0
+                ),
+              ),
+              Text(
+                "CLUBS",
+                style: TextStyle(
+                    fontFamily: "Lato",
+                    letterSpacing: 2.0
+                ),
+              ),
+              Text(
+                "MARKET",
+                style: TextStyle(
+                    fontFamily: "Lato",
+                    letterSpacing: 2.0
+                ),
+              ),
+              Text(
+                "DONATE",
+                style: TextStyle(
+                    fontFamily: "Lato",
+                    letterSpacing: 2.0
+                ),
+              )
+            ],
+          ),
+        ),
+        body: TabBarView(
+            controller: _tabController, children: [tabPage(0), tabPage(1), tabPage(2), tabPage(3)]),
+      ),
+    );
+  }
+}
