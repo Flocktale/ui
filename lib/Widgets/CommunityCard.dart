0b4ad4d8af30c6b56089988cf0c8757955c00959
Utkarsh2524992 <utkarsh2524992@gmail.com>
2021-04-22 01:47:36 +0530

    Club creation not working

--- lib/Widgets/CommunityCard.dart
+++ lib/Widgets/CommunityCard.dart
@@ -114,9 +114,19 @@ class _CommunityCardState extends State<CommunityCard> {
                 width: size.width,
                 child: ListView.builder(
                     scrollDirection: Axis.horizontal,
-                    itemCount: 5,
+                    itemCount: widget.community.hosts!=null?widget.community.hosts.length>4?5:widget.community.hosts.length:0,
                     itemBuilder: (context, index) {
-                      return index == 4
+                      return widget.community.hosts.length>4?
+                      Container(
+                        margin: EdgeInsets.fromLTRB(0, 0, 5, 0),
+                        height: size.height / 22.5,
+                        width: size.height / 22.5,
+                        child: CustomImage(
+                          image: _userAvatar,
+                          radius: 2,
+                        ),
+                      ):
+                      index == 4
                           ? Container(
                               margin: EdgeInsets.fromLTRB(0, 0, 5, 0),
                               height: size.height / 22.5,
@@ -126,7 +136,7 @@ class _CommunityCardState extends State<CommunityCard> {
                                   color: Colors.black45),
                               child: Center(
                                 child: Text(
-                                  "+6",
+                                  (widget.community.hosts.length-4).toString(),
                                   style: TextStyle(
                                       fontFamily: "Lato", color: Colors.white),
                                 ),
@@ -153,7 +163,7 @@ class _CommunityCardState extends State<CommunityCard> {
                     child: Row(
                       children: [
                         Text(
-                          "15",
+                          widget.community.liveClubCount.toString(),
                           style: TextStyle(
                               fontFamily: "Lato", color: Colors.white),
                         ),
@@ -183,7 +193,7 @@ class _CommunityCardState extends State<CommunityCard> {
                     child: Row(
                       children: [
                         Text(
-                          "35",
+                          widget.community.scheduledClubCount.toString(),
                           style: TextStyle(
                               fontFamily: "Lato", color: Colors.white),
                         ),
@@ -212,7 +222,7 @@ class _CommunityCardState extends State<CommunityCard> {
                   child: Row(
                     children: [
                       Text(
-                        "15K",
+                        widget.community.memberCount.toString(),
                         style: TextStyle(
                             fontFamily: "Lato",
                             color: Colors.white,
@@ -223,21 +233,6 @@ class _CommunityCardState extends State<CommunityCard> {
                         color: Colors.white,
                         size: size.width / 25,
                       ),
-                      IconButton(
-                          icon: isMember
-                              ? Icon(
-                                  Icons.check,
-                                  color: Colors.green,
-                                )
-                              : Icon(
-                                  Icons.add,
-                                  color: Colors.white,
-                                ),
-                          onPressed: () {
-                            setState(() {
-                              isMember = !isMember;
-                            });
-                          })
                     ],
                   ),
                 ))
